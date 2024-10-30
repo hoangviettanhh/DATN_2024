e5d37ac8027fd2969a13252dc46bfa9d555b057a
vietanh <vietanhhoang021102@gmail.com>
2024-10-30 17:37:13 +0700

    homecomponent

--- frontend/src/app/client/home/home.component.ts
+++ frontend/src/app/client/home/home.component.ts
@@ -1,37 +1,52 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { CommonModule } from '@angular/common'; // Import CommonModule
+import { CommonModule } from '@angular/common';
 import { HomeService } from './home.service';
+import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [RouterModule, CommonModule],
+  imports: [RouterModule, CommonModule, ReactiveFormsModule],
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css'],
 })
-export class HomeComponent {
-  products: any[] = []; // Biến để lưu danh sách sản phẩm
-  loading: boolean = true; 
+export class HomeComponent implements OnInit {
+  products: any[] = [];  // Danh sách sản phẩm
+  loading: boolean = true;
   errorMessage: string = '';
+  priceForm: FormGroup;
 
-  constructor(private homeService: HomeService) {}
+  constructor(private homeService: HomeService, private fb: FormBuilder) {
+    this.priceForm = this.fb.group({
+      minPrice: [null],
+      maxPrice: [null],
+    });
 
-  ngOnInit() {
-    this.loadProducts(); //loadProducts khi component khởi tạo
   }
 
-  loadProducts() {
-    this.homeService.getProducts().subscribe(
+  ngOnInit() {
+    this.loadProducts();
+  }
+
+  applyPriceFilter() {
+    this.loading = true;
+    const { minPrice, maxPrice } = this.priceForm.value;
+
+    this.homeService.getProducts(minPrice, maxPrice).subscribe(
       (response) => {
-        this.products = response; 
+        this.products = response.product;
         this.loading = false;
       },
       (error) => {
         console.error('Lỗi khi tải sản phẩm:', error);
-        this.errorMessage = 'Không thể tải sản phẩm. Vui lòng thử lại sau.'; // Ghi lại thông báo lỗi
-        this.loading = false; 
+        this.errorMessage = 'Không thể tải sản phẩm. Vui lòng thử lại sau.';
+        this.loading = false;
       }
     );
+  }
+
+  loadProducts() {
+    this.applyPriceFilter();
   }
 }
